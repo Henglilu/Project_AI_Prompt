286618c4ca710ce7900b76d13931fff620035fa1
Henglilu <imsprea@gmail.com>
2024-08-03 16:29:02 +0700

    Third fix

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,52 +4,58 @@ import { useState, useEffect } from "react";
 
 import PromptCard from "./PromptCard";
 
-  const PromptCardList = ({data, handleTagClick}) => {
-    return (
-      <div className="mt-16 prompt_layout">
-              {data.map((post) => (
-      <PromptCard
-      key={post._id}
-      post={post}
-      handleTagClick={handleTagClick}
-      />
-    ))}
-      </div>
-    )
-  }
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
-  const Feed = () => {
-    const [searchText, setSearchText] = useState('');  // user type 
-    const [posts, setPosts] = useState([]);
-    const [filteredPosts, setFilteredPosts] = useState ([]) // to hold the filtered results
+const Feed = () => {
+  const [searchText, setSearchText] = useState(""); // user type
+  const [posts, setPosts] = useState([]);
+  const [filteredPosts, setFilteredPosts] = useState([]); // to hold the filtered results
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch('api/prompt');
+      const response = await fetch("/api/prompt");
       const data = await response.json();
 
       setPosts(data); // take every post
       setFilteredPosts(data); // take filtered posts
-    }
+    };
 
     fetchPosts();
-  }, [])
+  }, []);
 
   const handleSearchChange = (e) => {
     const newSearchText = e.target.value;
     setSearchText(newSearchText);
-    filterPosts(newSearchText)
+    filterPosts(newSearchText);
   };
 
   const filterPosts = (searchText) => {
     const filtered = posts.filter((post) => {
-      const promptMatch = post.prompt.toLowerCase().includes(searchText.toLowerCase());
-      const tagMatch = post.tag.toLowerCase().includes(searchText.toLowerCase());
-      const usernameMatch = post.creator.username.toLowerCase().includes(searchText.toLowerCase());
+      const promptMatch = post.prompt
+        .toLowerCase()
+        .includes(searchText.toLowerCase());
+      const tagMatch = post.tag
+        .toLowerCase()
+        .includes(searchText.toLowerCase());
+      const usernameMatch = post.creator.username
+        .toLowerCase()
+        .includes(searchText.toLowerCase());
       return promptMatch || tagMatch || usernameMatch;
-    })
+    });
     setFilteredPosts(filtered);
-  }
+  };
 
   return (
     <section className="feed">
@@ -64,10 +70,7 @@ import PromptCard from "./PromptCard";
           className="search_input peer"
         />
       </form>
-      <PromptCardList
-        data={filteredPosts}
-        handleTagClick={() => {}}
-      />
+      <PromptCardList data={filteredPosts} handleTagClick={() => {}} />
     </section>
   );
 };
